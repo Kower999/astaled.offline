b3ac7271bab8b23feba5c86e7f37e88b26147299
Kower999 <kower99@gmail.com>
2016-06-10 10:22:03 +0200

    1.832

--- config/astaled.settings.inc.php
+++ config/astaled.settings.inc.php
@@ -1,18 +1,19 @@
 <?php
+if(ENT_XML1 != 16) { define('ENT_XML1', 16); }
 
-if(ENT_XML1 != 16) {
-    define('ENT_XML1', 16);            
-}
+define('_ASTALED_UPDATE_',              true);
 
 define('_ASTALED_ADMIN_MAIL_',          'kower99@gmail.com');
 if(_PS_MODE_DEV_) {
-    define('_PS_ONLINE_MAIL_',              'kower99@gmail.com');        
+    define('_PS_ONLINE_MAIL_',          'kower99@gmail.com');        
 } else {
-    define('_PS_ONLINE_MAIL_',              'marian.gabris.vega@gmail.com');    
+    define('_PS_ONLINE_MAIL_',          'marian.gabris.vega@gmail.com');    
 }
 
 define('_ASTALED_ONLINE_',              'http://astaled.sk');
 define('_ASTALED_OFFLINE_',             'AstaledOffline');
+//define('_ASTALED_ONLINE_',              'http://obchod.astaled.sk');
+//define('_ASTALED_OFFLINE_',             'BaterkyOffline');
 
 
 define('_ASTALED_SENDER_MAIL_',         'astaledonline@gmail.com');
@@ -33,14 +34,12 @@ define('_PS_ONLINE_DOWNLOAD_',         _ASTALED_ONLINE_.'/download/');
 
 define('_PS_ADMIN_IMPORT_',            _PS_ROOT_DIR_.'/shopadmin/import/');
 
-define('_PS_ONLINE_DOWNLOAD_STOCK_UPDATES_',         _PS_ONLINE_DOWNLOAD_ . 'updates/stock_updates/');
-define('_PS_STOCK_UPDATES_DIR_',            _PS_DOWNLOAD_DIR_.'updates/stock_updates/');
+define('_PS_ONLINE_DOWNLOAD_STOCK_UPDATES_', _PS_ONLINE_DOWNLOAD_ . 'updates/stock_updates/');
+define('_PS_STOCK_UPDATES_DIR_',        _PS_DOWNLOAD_DIR_.'updates/stock_updates/');
 
-define('_PS_ONLINE_DOWNLOAD_XML_',         _ASTALED_ONLINE_ . '/');
+define('_PS_ONLINE_DOWNLOAD_XML_',      _ASTALED_ONLINE_ . '/');
 
-define('_PS_WAMP_DIR_', realpath(_PS_ROOT_DIR_.'/../bin/'));
+define('_PS_WAMP_DIR_',                 realpath(_PS_ROOT_DIR_.'/../bin/'));
 
-define('_PS_ONLINE_SQL_DOWNLOAD_',         _PS_ONLINE_DOWNLOAD_.'updates/sql_updates/');
-define('_PS_ONLINE_PHP_DOWNLOAD_',         _PS_ONLINE_DOWNLOAD_.'updates/onetime_php/');
-
-
+define('_PS_ONLINE_SQL_DOWNLOAD_',      _PS_ONLINE_DOWNLOAD_.'updates/sql_updates/');
+define('_PS_ONLINE_PHP_DOWNLOAD_',      _PS_ONLINE_DOWNLOAD_.'updates/onetime_php/');
